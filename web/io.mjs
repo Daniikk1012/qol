97bf7802469aee80ec1107030682cff01a90eed8
Daniikk1012 <daniyarsar@gmail.com>
2024-11-29 02:35:24 +0500

    Fix web character escaping

--- web/io.mjs
+++ web/io.mjs
@@ -1,11 +1,22 @@
 const outputElement = document.getElementById('output');
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+    .replace(/ /g, '&nbsp;')
+    .replace(/\n/g, '<br>');
+}
+
 export function output(s) {
-  outputElement.innerText += s;
+  outputElement.innerHTML += escapeHtml(s);
 }
 
 export function input() {
   const line = prompt('Enter a line of input:');
-  outputElement.innerText += line + '\n';
+  outputElement.innerHTML += escapeHtml(line + '\n');
   return line;
 }
